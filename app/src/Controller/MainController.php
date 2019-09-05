0bbcf5eac2d403d037be5349fc0c5ba82eb8f4ed
Dmitriy Machin <dmitriy.m@payop.com>
2019-09-05 15:36:10 +0300

    Finalize integration

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -12,9 +12,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\Cache\ItemInterface;
 
+/**
+ * Class MainController
+ * @package App\Controller
+ */
 class MainController extends AbstractController
 {
     /**
@@ -74,23 +76,14 @@ class MainController extends AbstractController
         string $orderId,
         OrderRepository $or,
         PayopClient $client,
-        CacheInterface $pool,
         EntityManagerInterface $em
     ) : Response {
         $order = $or->find($orderId);
-
-        $paymentMethods = $pool->get('paymentMethods', function(ItemInterface $item) use ($client) {
-            $item->expiresAt((new \DateTimeImmutable())->modify('+5 min'));
-
-            return $client->getPaymentMethods();
-        });
+        $paymentMethods = $client->getPaymentMethods();
 
         if ($request->isMethod(Request::METHOD_POST) && $request->request->has('pm')) {
             $pmId = $request->request->get('pm');
             $fields = $request->request->get('fields', []);
-            // IP it's required field for payer.
-            // It's detected automatically on the Payop side, or we can pass it manually
-            $fields['ip'] = $request->getClientIp();
 
             $resultUrl = $this->generateUrl('order-result', [
                 'id' => $orderId,
@@ -129,10 +122,140 @@ class MainController extends AbstractController
 
     /**
      * @Route("/order/{orderId}/before-payment-transaction", name="before-payment-transaction")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param string $orderId
+     * @param \App\Repository\OrderRepository $or
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \App\Service\PayopClient $client
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\Cache\InvalidArgumentException
      */
-    public function beforeCreatePaymentTransaction(string $orderId) : Response
-    {
-        $t = 1;
+    public function beforeCreatePaymentTransaction(
+        Request $request,
+        string $orderId,
+        OrderRepository $or,
+        EntityManagerInterface $em,
+        PayopClient $client
+    ) : Response {
+        $order = $or->find($orderId);
+        // while we have created invoice with selected payment method, we can decide what to do next.
+        // There available a two cases, depends on selected payment method.
+        // 1. if paymentMethod.formType is "cards" - create transaction request require Card Token. So next step is - card form
+        // 2. if paymentMethod.formType is not "cards" - we can make request to create transaction.
+        $invoice = $client->getInvoice($order->getPayopInvoiceId());
+        if ($invoice['paymentMethod']['formType'] === 'cards') {
+            return $this->redirectToRoute('bank-card', ['orderId' => $orderId]);
+        }
+
+        // create transaction
+        $this->createTransaction($request, $or, $em, $client, $orderId, null);
+
+        return $this->redirectToRoute('transaction-status', ['orderId' => $orderId]);
+    }
+
+    /**
+     * Create card token
+     *
+     * @Route("/order/{orderId}/bank-card", name="bank-card")
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\Repository\OrderRepository $or
+     * @param \App\Service\PayopClient $client
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param string $orderId
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function cardForm(
+        Request $request,
+        OrderRepository $or,
+        PayopClient $client,
+        EntityManagerInterface $em,
+        string $orderId
+    ) {
+        $order = $or->find($orderId);
+
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $tokenData = $client->createCardToken(
+                $order->getPayopInvoiceId(),
+                $request->request->all()
+            );
+            $this->createTransaction($request, $or, $em, $client, $orderId, $tokenData['token']);
+
+            return $this->redirectToRoute('transaction-status', ['orderId' => $orderId]);
+        }
+
+        return $this->render('card-form.html.twig', [
+            // Test card - it's not real
+            'pan' => '5555555555554444',
+            'expirationDate' => '12/20',
+            'cvv' => '123',
+            'holderName' => 'Makar Makarenko',
+        ]);
+    }
+
+    /**
+     * Just show result page
+     *
+     * @Route("/order/{orderId}/tx-status", name="transaction-status")
+     *
+     * @param string $orderId
+     * @param \App\Repository\OrderRepository $or
+     * @param \App\Service\PayopClient $client
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function checkTransactionStatus(
+        string $orderId,
+        OrderRepository $or,
+        PayopClient $client
+    ) : Response {
+        $order = $or->find($orderId);
+        $statusResponse = $client->checkTransactionStatus($order->getPayopTxid());
+        if (!empty($statusResponse['form'])) {
+            if ($statusResponse['form']['method'] === Request::METHOD_GET) {
+                return $this->redirect($statusResponse['form']['url']);
+            }
+
+            return $this->render('status-post.html.twig', ['form' => $statusResponse['form']]);
+        }
+
+        if ($statusResponse['status'] === 'pending') {
+            sleep(10);
+            return $this->redirectToRoute('transaction-status', ['orderId' => $orderId]);
+        }
+
+        if (in_array($statusResponse['status'], ['success', 'fail'])) {
+            return $this->redirect($statusResponse['url']);
+        }
+
+        throw new \RuntimeException('Ooops ...');
+
+        // Based on above response we can choose several ways what to do next:
+        // !!! Important: The list is sorted by importance of checks.
+        // 1. $statusResponse['form'] is not empty - redirect user (GET/POST) to $statusResponse['form']['url'].
+        //    Usually POST request - this is payer bank 3DS page.
+        //    So you have to send form with enctype='application/x-www-form-urlencoded' attribute
+        //    and this request should be InBrowser (Normal POST request: https://stackoverflow.com/a/15262442/2090853)
+        //    $statusResponse['form'] can has next structure:
+        //    ['url' => url where make request, 'method' => 'http method GET|POST', 'fields' => [array with formFieldName => formFieldValue]]
+        //      Example for GET: ['url' => 'https://pay.skrill.com/app/?sid=9345093478', 'method' => 'GET', 'fields' => []]
+        //      Example for POST:
+        //          [
+        //              'url' => 'https://acs.anybank.com/',
+        //              'method' => 'POST',
+        //             'fields' => ['PaReq' => 'fmn3o8usfjlils', 'MD' => '8ec777d6-685d-4e06-b356-d7673acb47ba', 'TermUrl' => 'https://payop.com/v1/url']
+        //          ]
+        // 2. $statusResponse['status'] is "pending" - repeat transaction status request after 5-10 seconds.
+        // 3. $statusResponse['status'] is "success" - redirect to $statusResponse['url']
+        // 4. $statusResponse['status'] is "fail" - redirect to $statusResponse['url']
+        // 5. Exceptional case. Something went wrong on the Payop side. Contact our support.
     }
 
     /**
@@ -159,5 +282,53 @@ class MainController extends AbstractController
             'order' => $order,
             'state' => $state,
         ]);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\Repository\OrderRepository $or
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \App\Service\PayopClient $client
+     * @param string $orderId
+     * @param string|null $cardToken
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    private function createTransaction(
+        Request $request,
+        OrderRepository $or,
+        EntityManagerInterface $em,
+        PayopClient $client,
+        string $orderId,
+        ?string $cardToken
+    ) : void {
+        $order = $or->find($orderId);
+        // While we have invoice we can get data for checkout from this invoice
+        // or we can make page where payer can provide all this data.
+        // Here we using first case.
+        $invoice = $client->getInvoice($order->getPayopInvoiceId());
+        $customer = $invoice['payer']['extraFields'];
+        $customer['email'] = $invoice['payer']['email'];
+        // IP is required field for payer.
+        // It's detected automatically on the Payop side, or we can pass it manually
+        // But in case of using integration Server-To-Server you have to pass it.
+        $customer['ip'] = $request->getClientIp();
+        $checkStatusUrl = $this->generateUrl(
+            'transaction-status',
+            ['orderId' => $orderId],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $transaction = $client->createTransaction(
+            $invoice['identifier'],
+            $checkStatusUrl,
+            $customer,
+            $cardToken
+        );
+
+        $order->setPayopTxid($transaction['txid']);
+        $em->persist($order);
+        $em->flush();
     }
 }
--- app/src/Service/PayopClient.php
+++ app/src/Service/PayopClient.php
@@ -6,6 +6,8 @@ use App\Entity\Order;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\RequestOptions;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * Class PayopClient
@@ -40,38 +42,54 @@ class PayopClient
     private $accessToken;
 
     /**
+     * @var \Symfony\Contracts\Cache\CacheInterface
+     */
+    private $cachePool;
+
+    /**
      * @param \GuzzleHttp\ClientInterface $client
      * @param string $publicKey
      * @param string $secretKey
      * @param string $accessToken
+     * @param \Symfony\Contracts\Cache\CacheInterface $cachePool
      */
     public function __construct(
         ClientInterface $client,
         string $publicKey,
         string $secretKey,
-        string $accessToken
+        string $accessToken,
+        CacheInterface $cachePool
     ) {
         $this->client = $client;
         $this->publicKey = $publicKey;
         $this->secretKey = $secretKey;
         $this->accessToken = $accessToken;
+        $this->cachePool = $cachePool;
     }
 
     /**
      * Get merchant payment methods
      *
      * @return array
+     *
+     * @throws \Psr\Cache\InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function getPaymentMethods() : array
     {
-        $response = $this->client->request(
-            Request::METHOD_GET,
-            'instrument-settings/payment-methods/available-for-user',
-            [RequestOptions::HEADERS => ['token' => $this->accessToken]]
-        );
+        $paymentMethods = $this->cachePool->get('paymentMethods', function(ItemInterface $item) {
+            $item->expiresAt((new \DateTimeImmutable())->modify('+5 min'));
 
-        return json_decode($response->getBody()->getContents(), true)['data'];
+            $response = $this->client->request(
+                Request::METHOD_GET,
+                'instrument-settings/payment-methods/available-for-user',
+                [RequestOptions::HEADERS => ['token' => $this->accessToken]]
+            );
+
+            return json_decode($response->getBody()->getContents(), true)['data'];
+        });
+
+        return $paymentMethods;
     }
 
     /**
@@ -91,6 +109,14 @@ class PayopClient
         $failUrl,
         ?string $pmId
     ) : string {
+        // "payer" is a structure with a specific set of fields
+        // such as: email, name, phone, extraFields.
+        // "email" always required. Other fields depends on selected payment method.
+        // To avoid rigid binding to the structure,
+        // which does not give the entire possible list of fields to save all possible data
+        // we can use "extraFields" field to save payer extra fields.
+        $payer['extraFields'] = $payer;
+
         $params = [
             'publicKey' => $this->publicKey,
             'order' => [
@@ -125,6 +151,7 @@ class PayopClient
             [RequestOptions::JSON => $params]
         );
         $data = json_decode($response->getBody()->getContents(), true);
+
         // Here you have to handle possible errors from response
 
         return $data['data'];
@@ -134,25 +161,104 @@ class PayopClient
      * @param string $invoiceId
      *
      * @return array
+     *
+     * @throws \Psr\Cache\InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function getInvoice(string $invoiceId) : array
     {
+        $invoice = $this->cachePool->get("invoice_{$invoiceId}", function(ItemInterface $item) use ($invoiceId) {
+            $item->expiresAt((new \DateTimeImmutable())->modify('+5 min'));
+
+            $response = $this->client->request(
+                Request::METHOD_GET,
+                "invoices/{$invoiceId}",
+                [RequestOptions::HEADERS => ['token' => $this->accessToken]]
+            );
+
+            return json_decode($response->getBody()->getContents(), true)['data'];
+        });
+
+        return $invoice;
+    }
+
+    /**
+     * @param string $invoiceId
+     * @param string $checkStatusUrl
+     * @param array $customer
+     * @param string|null $cardToken
+     *
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function createTransaction(
+        string $invoiceId,
+        string $checkStatusUrl,
+        array $customer,
+        ?string $cardToken
+    ) : array {
+        $params = [
+            'invoiceIdentifier' => $invoiceId,
+            'customerData' => $customer,
+            'cardToken' => $cardToken,
+            'checkStatusUrl' => $checkStatusUrl,
+        ];
+
+        $response = $this->client->request(
+            Request::METHOD_POST,
+            'checkout/create',
+            [RequestOptions::JSON => $params]
+        );
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        // Here you have to handle possible errors from response
+
+        return $data['data'];
+    }
+
+    /**
+     * @param string $invoiceId
+     * @param array $card
+     *
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function createCardToken(string $invoiceId, array $card) : array
+    {
+        $params = [
+            'invoiceIdentifier' => $invoiceId,
+            'pan' => $card['pan'],
+            'expirationDate' => $card['expirationDate'],
+            'cvv' => $card['cvv'],
+            'holderName' => $card['holderName'],
+        ];
+
+        $response = $this->client->request(
+            Request::METHOD_POST,
+            'payment-tools/card-token/create',
+            [RequestOptions::JSON => $params]
+        );
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        // Here you have to handle possible errors from response
+
+        return $data['data'];
+    }
+
+    /**
+     * @param string $txid
+     *
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function checkTransactionStatus(string $txid) : array
+    {
         $response = $this->client->request(
             Request::METHOD_GET,
-            "/v1/invoices/{$invoiceId}",
-            [RequestOptions::HEADERS => ['token' => $this->accessToken]]
+            "checkout/check-transaction-status/{$txid}"
         );
 
         return json_decode($response->getBody()->getContents(), true)['data'];
-    }
-
-    public function createTransaction(
-        string $invoiceId,
-        string $checkStatusUrl
-    )
-    {
-
     }
 
     /**
